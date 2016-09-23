07e80cc8cc40af8c3e9f45fd60795a40175628b8
han201 <younghan.lee@gmail.com>
2016-09-22 23:30:14 -0400

    11 solved
    The upper right to lower left diagonal was most confusing and
    time-consuming

--- EulerProject11_data_with_np.py
+++ EulerProject11_data_with_np.py
@@ -19,6 +19,7 @@ numCols = len(fhand[0, :])
 print "Number of rows = ", numRows, " and number of columns =", numCols
 
 #1. for left and right maximum
+maximum = 1
 for rows in range(0, numRows):
     for i in range(0, numCols-numOfMul+1):
         product = 1
@@ -26,9 +27,10 @@ for rows in range(0, numRows):
             product = product * fhand[rows, j]
         if (maximum < product):
             maximum = product
-            print "Max is for left and right: ", fhand[rows, i:i+numOfMul], product
-
+            print "Max is for left and right: for row and i ", rows, i, fhand[rows, i:i+numOfMul], product
+print "Done with left to right\n"
 #2. for up and down maximum
+maximum = 1
 for cols in range(0, numCols):
     for i in range(0, numRows-numOfMul+1):
         product = 1
@@ -36,9 +38,10 @@ for cols in range(0, numCols):
             product = product*fhand[j, cols]
         if (maximum < product):
             maximum = product
-            print "Max is for up and down: ", fhand[i:i+numOfMul, cols], product
-
+            print "Max is for up and down: for cols and i ", cols, i, fhand[i:i+numOfMul, cols], product
+print "Done with up and down\n"
 #3. Lower Triangular maximum (including Diagonal)
+maximum = 1
 numDiagonal=numRows   # inlude the diagonal
 
 while (numDiagonal>= numOfMul):
@@ -49,12 +52,13 @@ while (numDiagonal>= numOfMul):
             product = product * fhand[j, j-(numRows-numDiagonal)]
         if (maximum < product):
             maximum = product
-            print "Max is for Lower Triangular: ", fhand[j-3, j-(numRows-numDiagonal)-3], fhand[j-2, j-(numRows-numDiagonal)-2], fhand[j-1, j-(numRows-numDiagonal)-1], fhand[j, j-(numRows-numDiagonal)], product
+            print "Max is for Lower Tri when numDiagonal & i: ", numDiagonal, i, fhand[j-3, j-(numRows-numDiagonal)-3], fhand[j-2, j-(numRows-numDiagonal)-2], fhand[j-1, j-(numRows-numDiagonal)-1], fhand[j, j-(numRows-numDiagonal)], product
     numDiagonal-=1
 
+print "Done with Left to Right Lower diagonal\n"
 #4. Upper Triangular maximum: need to use while
+maximum = 1
 numDiagonal=numCols-1     # exclude diagonal
-
 while (numDiagonal>= numOfMul):
     for i in range(numCols-numDiagonal, numCols - numOfMul + 1):
         product = 1
@@ -62,6 +66,37 @@ while (numDiagonal>= numOfMul):
             product = product * fhand[j-(numCols-numDiagonal), j]
         if (maximum < product):
             maximum = product
-            print "Max is for Upper Triangular: ", fhand[j-(numCols-numDiagonal)-3, j-3], fhand[j-(numCols-numDiagonal)-2, j-2], fhand[j-(numCols-numDiagonal)-1, j-1], fhand[j-(numCols-numDiagonal), j], product
+            print "Max is for Upper Tri when numDiagonal & i: ", numDiagonal, i, fhand[j-(numCols-numDiagonal)-3, j-3], fhand[j-(numCols-numDiagonal)-2, j-2], fhand[j-(numCols-numDiagonal)-1, j-1], fhand[j-(numCols-numDiagonal), j], product
+    numDiagonal-=1
+print "Done with Left to Right Upper diagonal\n"
+
+
+#5. Lower Triangular maximum (including Diagonal) (from Right to Left)
+maximum = 1
+numDiagonal=numRows   # inlude the diagonal
+while (numDiagonal>= numOfMul):
+    for i in range(numRows-numDiagonal, numRows - numOfMul + 1):
+        product = 1
+        for j in range(i, i+numOfMul):
+            product = product * fhand[j, (numRows-1)-j+(numRows-numDiagonal)]   # j-(numRows-numDiagonal)
+        if (maximum < product):
+            maximum = product
+            print "Max is for Lower Tri when numDiagonal & i: ", numDiagonal, i, fhand[j-3, (numRows-1)-j+(numRows-numDiagonal)+3], fhand[j-2, (numRows-1)-j+(numRows-numDiagonal)+2], fhand[j-1, (numRows-1)-j+(numRows-numDiagonal)+1], fhand[j, (numRows-1)-j+(numRows-numDiagonal)], product
     numDiagonal-=1
 
+print "Done with Right to Left Lower diagonal\n"
+
+
+#6. Upper Triangular maximum: need to use while (from Right to Left)
+maximum = 1
+numDiagonal=numCols-1     # exclude diagonal
+while (numDiagonal>= numOfMul):
+    for i in range(numCols-numDiagonal, numCols - numOfMul + 1):
+        product = 1
+        for j in range(i, i+numOfMul):
+            product = product * fhand[j-(numCols-numDiagonal), (numCols-1)-j]   # fhand[j-(numCols-numDiagonal), j]
+        if (maximum < product):
+            maximum = product
+            print "Max is for Upper Tri when numDiagonal & i: ", numDiagonal, i, fhand[j-(numCols-numDiagonal)-3, (numCols-1)-j+3] , fhand[j-(numCols-numDiagonal)-2, (numCols-1)-j+2] , fhand[j-(numCols-numDiagonal)-1, (numCols-1)-j+1] , fhand[j-(numCols-numDiagonal), (numCols-1)-j] , product
+    numDiagonal-=1
+print "Done with Right to Left Upper diagonal\n"
